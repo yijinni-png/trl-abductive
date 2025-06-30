950f2de2dff22f1d5ab4042f888744522ee5876a
yijin-ni <yijin.ni@orby.ai>
2025-06-30 04:17:55 +0000

    Add ADPOTrainer and ADPOConfig

--- trl/__init__.py
+++ trl/__init__.py
@@ -157,6 +157,8 @@ if TYPE_CHECKING:
         DataCollatorForCompletionOnlyLM,
         DPOConfig,
         DPOTrainer,
+        ADPOConfig,
+        ADPOTrainer,
         FDivergenceConstants,
         FDivergenceType,
         GKDConfig,
